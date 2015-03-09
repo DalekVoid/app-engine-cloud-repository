4d6f5506e8a33c4906340b7a8008ee4722db704e
Dalek Void <dalek@voidamplifier.com>
2015-03-09 22:44:58 +0800

    slight changes

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func init() {
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "You can view the source code here: ")
+  fmt.Fprint(w, "You can view the source code here: http://github.com")
 }
