61b134665c898dccec871f3fceed41ce4d492d01
Dalek Void <dalek@voidamplifier.com>
2015-03-09 22:39:40 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+        "net/http"
+        "fmt"
+)
+
+func init() {
+        http.HandleFunc("/", root)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "You can view the source code here: ")
+}
